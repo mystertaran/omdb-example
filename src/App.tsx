090d58d5894581df2f5f8babdc774fd871e31029
myster_taran <sz.taranczewski@gmail.com>
2024-10-10 21:33:54 +0200

    preparing for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, {Suspense, lazy} from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from "./pages/Home/index";
-import Details from "./pages/Details/index";
 import {Provider} from "react-redux";
 import {store} from "./store";
+import Loader from "./components/Loader";
+
+const Home = lazy(() => import("./pages/Home"));
+const Details = lazy(() => import("./pages/Details"));
 
 const App: React.FC = () => {
     return (
         <Provider store={store}>
             <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/movie/:id" element={<Details />} />
-                </Routes>
+                <Suspense fallback={<Loader />}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/movie/:id" element={<Details />} />
+                    </Routes>
+                </Suspense>
             </Router>
         </Provider>
     );
